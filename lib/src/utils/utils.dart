0ff50937394aad80b8c4946c7a1140af13007f2e
Si Thu Phyo <createcodes155@gmail.com>
2021-05-18 12:17:50 +0630

    Updated and fixed some issues

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -4,5 +4,5 @@ double calculateAspectRatio(BuildContext context, Size screenSize) {
   final width = screenSize.width;
   final height = screenSize.height;
   // return widget.playOptions.aspectRatio ?? controller.value.aspectRatio;
-  return width > height ? width / height : height / width;
+  return 1;
 }
